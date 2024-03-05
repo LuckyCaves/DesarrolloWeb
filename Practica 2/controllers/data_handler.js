a3687e6f2868d47edb7259c96fda55ccbb00b435
TadeoQuiroz <tadeo.quiroz@celularesfuturo.com>
2024-03-04 18:19:30 -0600

    Practica 2 Creación

---
+++ Practica 2/controllers/data_handler.js
@@ -0,0 +1,45 @@
+class Products
+{
+
+    constructor()
+    {
+        this.products = [];
+    }
+
+    addProduct(product)
+    {
+        this.products.push(product);
+    }
+
+    getProducts()
+    {
+        return this.products;
+    }
+
+    getProductById(uuid)
+    {
+        return this.products.find(product => product.uuid === uuid);
+    }
+
+    createProduct(product)
+    {
+        let newProduct = new Product(product.uuid, product.title, product.description, product.imageUrl, product.unit, product.stock, product.pricePerUnit, product.category);
+        this.addProduct(newProduct);
+    }
+
+    updateProduct(uuid, updatedProduct)
+    {
+        this.deleteProduct(uuid);
+        let newProduct = new Product(updatedProduct.uuid, updatedProduct.title, updatedProduct.description, updatedProduct.imageUrl, updatedProduct.unit, updatesProduct.stock, updatedProduct.pricePerUnit, updatedProduct.category);
+        this.addProduct(updatedProduct);
+    }
+
+    deleteProduct(uuid)
+    {
+        let index = this.products.findIndex(product => product.uuid === uuid);
+        this.products.splice(index, 1);
+    }
+
+    
+
+}
