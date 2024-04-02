c7f7c467273c3f2a80c8f5fc307962a20c80465e
LuckyCaves <quiroztadeo3@gmail.com>
2024-04-01 22:13:14 -0600

    Clase 11 y 12

--- Clase 10/server.mjs
+++ Clase 10/server.mjs
@@ -1,10 +1,29 @@
 import express from 'express';
 import chalk from 'chalk';
 import * as fs from 'node:fs';
+import ascii_cats from 'ascii-cats';
 import cors from 'cors';
 
 const app = express();
 const port = 3000;
+
+function print_cat(req, res, next)
+{
+    console.log(ascii_cats());
+    next();
+}
+
+function print_info(req, res, next)
+{
+    console.log(chalk.blue('Método: '), chalk.green(req.method));
+    console.log(chalk.blue('URL: '), chalk.green(req.originalUrl));
+    console.log(chalk.blue('Fecha: '), chalk.green(new Date(Date.now()).toString()));
+    console.log(chalk.blue('Content-Type: '), chalk.green(req.headers['content-type']));
+    next();
+}
+
+app.use("/", print_cat);
+app.use("/users", print_info);
 
 app.use(cors({
     methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
@@ -14,8 +33,6 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
     console.log(chalk.green('Entró a raiz'));
 });
-
-
 
 app.get('/home', (req, res) => {
     res.send('Hello Home!');
@@ -32,7 +49,7 @@ app.get('/users', (req, res) => {
     }
     else
     {
-        fs.readFile('DesarrolloWeb/Clase 9/files/usersFile.txt', 'utf8', (error, data) => {
+        fs.readFile('DesarrolloWeb/Clase 11/files/usersFile.txt', 'utf8', (error, data) => {
             if(error)
                 console.log(error);
             else
